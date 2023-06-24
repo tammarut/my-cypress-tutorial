a19d69b9408802f2bc21564df582c0c13e42225a
Pleum Tammarut <tammarut@mercular.com>
2023-06-24 23:10:28 +0700

    🔧 config(ESLint): setup typescript-eslint/parser and show linting

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	root: true,
-	env: { 'cypress/globals': true },
-	plugins: ['eslint-plugin-cypress', 'chai-friendly'],
-	extends: ['plugin:cypress/recommended', 'plugin:chai-friendly/recommended'],
-}
